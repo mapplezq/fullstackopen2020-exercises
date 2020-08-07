3eaaa66445fca585e64ffe2050e38e0287a4a23b
Mapple <mapplezq@gmail.com>
2020-08-07 23:32:48 +0800

    phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,13 +2,28 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas', 
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newSearchName, setNewSearchName] = useState('')
+
+  const handleSearchNameChange = (event) => {
+    // event.preventDefault()
+
+    setNewSearchName(event.target.value)
+    // console.log(event.target.value);
+    
+    const copy = [...persons]
+    const result = copy.filter(person => 
+      person.name.includes(event.target.value)
+    )
+    console.log('result:', result);
+    setPersons(result)
+  }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -34,7 +49,12 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>    
+      <h2>Phonebook</h2>   
+      <div>
+        filter shown with <input type="text" value={newSearchName} onChange={handleSearchNameChange} />
+      </div>
+      
+      <h2>add a new</h2> 
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} 
